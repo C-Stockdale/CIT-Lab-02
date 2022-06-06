f724393d7239c12016dc14f0e362d38231e8db98
C-Stockdale <104946581+C-Stockdale@users.noreply.github.com>
2022-06-06 03:21:44 -0700

    Create Lab-02.js

---
+++ Lab-02.js
@@ -0,0 +1,10 @@
+/*
+    CIT 281 Lab 2dir
+    Name: Cory Stockdale
+*/
+function square(num) {
+  return num * num;
+}
+console.log("Square operations:");
+for (let i = 2; i <= 10; i += 2) {}
+console.log(`Square of ${i} is ${square(i)}`);
